77feb079293eb975d45ae2d4ec60d04a94f22c8d
David Cuadros <cuadrosdavid01@gmail.com>
2024-05-06 20:46:39 +0000

    creación entidades bd (Usuario y Reporte)

---
+++ demo/src/main/java/com/cyclistalert/demo/modeloDB/Usuario.java
@@ -0,0 +1,28 @@
+package com.cyclistalert.demo.modeloDB;
+
+import java.util.List;
+import java.util.ArrayList;
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Usuario {
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    Integer id;
+
+    String nombre;
+    String email;
+    Integer telefono;
+
+    @OneToMany(mappedBy = "usuarioID", cascade = CascadeType.ALL)
+    List <Reporte> reportes = new ArrayList<>();
+}
